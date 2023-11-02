34f660329c8133ecc2b7bf9ab83c5539308dba0f
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2023-11-02 12:42:48 -0400

    Delete Downloads/gurufox-ai-frontend-main directory

--- Downloads/gurufox-ai-frontend-main/README.md
+++
@@ -1,6 +0,0 @@
-# OurDate GuruFox Frontend
-
-|Technologies| Documentation |
-|-----|-----|
-|ReactJS/Vite|https://vitejs.dev/guide/|
-|React-Router-Dom v6.4|https://reactrouter.com/en/main/start/overview|
--- Downloads/gurufox-ai-frontend-main/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
